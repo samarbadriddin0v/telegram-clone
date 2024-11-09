7baca697070dc24218aab759f829fbcd9afe1949
samarbadriddin0v <sammibadriddinov@gmail.com>
2024-11-09 08:45:00 +0200

    №7. Database, error middleware & SMTP

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const { default: mongoose } = require('mongoose')
+const errorMiddleware = require('./middlewares/error.middleware')
 
 const app = express()
 
@@ -10,6 +12,16 @@ app.use(express.json())
 
 app.use('/api', require('./routes/index'))
 
-const PORT = process.env.PORT || 6000
+app.use(errorMiddleware)
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+const bootstrap = async () => {
+	try {
+		const PORT = process.env.PORT || 6000
+		mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB connected'))
+		app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+	} catch (error) {
+		console.error(error)
+	}
+}
+
+bootstrap()
--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,21 @@
+const BaseError = require('../errors/base.error')
+const userModel = require('../models/user.model')
+const mailService = require('../service/mail.service')
+
 class AuthController {
 	async login(req, res, next) {
-		const { email } = req.body
-		res.json({ email })
+		try {
+			const { email } = req.body
+			await mailService.sendOtp(email)
+			// const existUser = await userModel.findOne({ email })
+			// if (existUser) {
+			// 	throw BaseError.BadRequest('User already exist', [{ email: 'User already exist' }])
+			// }
+			// const createdUser = await userModel.create({ email })
+			res.status(201).json({ email })
+		} catch (error) {
+			next(error)
+		}
 	}
 	async verify(req, res, next) {
 		const { email, otp } = req.body
@@ -10,5 +24,3 @@ class AuthController {
 }
 
 module.exports = new AuthController()
-
-// CLIENT bilan SERVER data muloqot tili bu JSON formatda bo'ladi
