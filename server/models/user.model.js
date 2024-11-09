454ede82b2198758d54f7f1c12d3abe1b3fe0c21
samarbadriddin0v <sammibadriddinov@gmail.com>
2024-11-09 13:23:29 +0200

    №9. User api route

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -11,6 +11,7 @@ const userSchema = new Schema(
 		muted: { type: Boolean, default: false },
 		notificationSound: { type: String, default: 'notification.mp3' },
 		sendingSound: { type: String, default: 'sending.mp3' },
+		contacts: [{ type: Schema.Types.ObjectId, ref: 'User' }],
 	},
 	{ timestamps: true }
 )
--- server/routes/index.js
+++ server/routes/index.js
@@ -11,9 +11,21 @@ router.group('/auth', route => {
 })
 
 router.group('/user', route => {
+	route.get('/contacts', userController.getContacts)
 	route.get('/messages/:contactId', userController.getMessages)
 
-	route.post('/create-message', userController.createMessage)
+	route.post('/message', userController.createMessage)
+	route.post('/message-read', userController.messageRead)
+	route.post('/contact', userController.createContact)
+	route.post('/reaction', userController.createReaction)
+	route.post('/send-otp', userController.sendOtp)
+
+	route.put('/profile', userController.updateProfile)
+	route.put('/message/:messageId', userController.updateMessage)
+	route.put('/email', userController.updateEmail)
+
+	route.delete('/', userController.deleteUser)
+	route.delete('/message/:messageId', userController.deleteMessage)
 })
 
 module.exports = router
